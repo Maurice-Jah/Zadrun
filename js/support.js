4bfc233080e59c81b204755981c2868438e53b86
mauricebuchi <mauricejahwizwid@gmail.com>
2021-09-27 15:59:36 +0100

    create faq section

---
+++ js/support.js
@@ -0,0 +1,14 @@
+// function faqOpen() {
+//   var answer = document.getElementById("answer");
+//   const icon = document.querySelector(".fas");
+
+//   if (icon.classList.contains("fa-chevron-right")) {
+//     icon.classList.remove("fa-chevron-right");
+//     icon.classList.add("fa-chevron-down");
+//     // answer.style.display = "block";
+//   } else {
+//     icon.classList.remove("fa-chevron-down");
+//     icon.classList.add("fa-chevron-right");
+//     // answer.style.display = "none";
+//   }
+// }
