f2b8dcd21c8262a902d84be6f6f6e8ce4ca427c8
mauricebuchi <mauricejahwizwid@gmail.com>
2021-09-28 15:05:54 +0100

    create company.js and format support js

---
+++ js/company.js
@@ -0,0 +1,55 @@
+var slides = document.querySelectorAll(".company__slider");
+var btns = document.querySelectorAll(".company__navigation-btn");
+
+let currentSlide = 1;
+
+// manual
+var manualNav = function (manual) {
+  slides.forEach((slide) => {
+    slide.classList.remove("active");
+    btns.forEach((btn) => {
+      btn.classList.remove("active");
+    });
+  });
+
+  slides[manual].classList.add("active");
+  btns[manual].classList.add("active");
+};
+
+btns.forEach((btn, i) => {
+  btn.addEventListener("click", () => {
+    manualNav(i);
+    currentSlide = i;
+  });
+});
+
+// Automatic
+
+var repeat = function (activeClass) {
+  let active = document.getElementsByClassName("active");
+  let i = 1;
+
+  var repeater = () => {
+    setTimeout(function () {
+      [...active].forEach((activeSlide) => {
+        activeSlide.classList.remove("active");
+      });
+
+      slides[i].classList.add("active");
+      btns[i].classList.add("active");
+      i++;
+
+      if (slides.length == i) {
+        i = 0;
+      }
+
+      if (i >= slides.length) {
+        return;
+      }
+      repeater();
+    }, 10000);
+  };
+  repeater();
+};
+
+repeat();
--- js/support.js
+++ js/support.js
@@ -1,14 +1,16 @@
-// function faqOpen() {
-//   var answer = document.getElementById("answer");
-//   const icon = document.querySelector(".fas");
+var faq = document.getElementsByClassName("faq__faq-question");
+var i;
 
-//   if (icon.classList.contains("fa-chevron-right")) {
-//     icon.classList.remove("fa-chevron-right");
-//     icon.classList.add("fa-chevron-down");
-//     // answer.style.display = "block";
-//   } else {
-//     icon.classList.remove("fa-chevron-down");
-//     icon.classList.add("fa-chevron-right");
-//     // answer.style.display = "none";
-//   }
-// }
+var len = faq.length;
+
+for (i = 0; i < len; i++) {
+  faq[i].addEventListener("click", function () {
+    this.classList.toggle("active");
+    var answer = this.nextElementSibling;
+    if (answer.style.maxHeight) {
+      answer.style.maxHeight = null;
+    } else {
+      answer.style.maxHeight = answer.scrollHeight + "px";
+    }
+  });
+}
